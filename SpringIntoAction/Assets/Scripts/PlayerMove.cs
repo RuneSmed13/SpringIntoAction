5fda3708b3045e21b1f163181084a1f655d780e1
RuneSmed13 <rune.smedsrud@gmail.com>
2025-02-11 11:32:33 +0100

    noe ikke enda

--- SpringIntoAction/Assets/Scripts/PlayerMove.cs
+++ SpringIntoAction/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("I sould be talking");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
